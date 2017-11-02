933b6711ea6038899c8f7e4ebb0dbb8ee577358a
cis w24 <cisw24@BCT.msac.mtsac.edu>
2017-11-01 21:22:59 -0700

    'kkk'

---
+++ 11.01.2017/personAgain.py
@@ -0,0 +1,14 @@
+#basic person with first and last name
+# 11/1/2017
+class Person:
+	
+	def __init__(self, first, last):
+		self.first_name = first
+		self.last_name = last
+		
+	def __str__(self):
+		return self.first_name + " " + self.last_name
+
+		
+	def getLastName(self):
+		return self.last_name
