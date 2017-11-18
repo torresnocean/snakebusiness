8bab2cb4a98d5b46b730f6d987d0d269f64ace71
dvsocean <daniel.ocean@aa.com>
2017-11-17 22:59:20 -0800

    doing homework on the plane

---
+++ Comments/comments.py
@@ -0,0 +1,10 @@
+class Comment:
+
+	def setId(self,  id):
+		self.commentId = id
+
+	def setContent(self, content):
+		self.content = content
+
+	def setTimeStamp(self, timeStamp):
+		self.timeStamp = timeStamp
