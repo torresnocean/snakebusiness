c1202c8694ca85662763c909b18cc2ee894950f3
htorres6 <Humbertot31@gmail.com>
2017-11-21 18:06:32 -0800

    New uptade on my Comment.py

--- myWork/TorresHComment.py
+++ myWork/TorresHComment.py
@@ -3,40 +3,50 @@
 # CISW 24
 # Program 5
 
-# creating a class "comment"..
-import time
+# Creating a class Comment to print out an ID, content, and timestamp.. 
+from time import strftime
 
 class Comment:
 
-	def setId(self, post_id):
-		self.post_id = post_id
-
-	def getId(self):
-		return self.post_id
+# The setter..
+	def setId(self, set_id):
+		self.set_id = set_id
 
 	def setContent(self, content):
 		self.content = content
 
+# I used 1 argument (time_stamp) and it worked..
+	def setTimeStamp(self, time_stamp):
+		self.time_stamp =  strftime('%A, %b. %d %Y   %H:%M:%S %Z')
+	
+# The getter..
+	def getId(self):
+		return self.set_id
+	
 	def getContent(self):
 		return self.content
 
-	def setTime(self, time):
-		self.time = time
+	def getTimeStamp(self):
+		return self.time_stamp
 
-	def getTime(self):
-		return self.time
-
-	def __str__(self):
-		return self.post_id + " " + self.content + " " + self.time
-
-# testing section
+# Testing section..
 if __name__ == '__main__':
+	obj = Comment()
 
-	c = Comment()
-	print("Hello.. Good job so far..")
+# Testing the unique 'id'
+	obj.setId(10)
+	print('Unique Id:', obj.getId())
+	print()
 
-	print(c)
+# Testing the 'content'
+	obj.setContent('[This test content] is the content.')
+	print(obj.getContent())
+	print()
 
+# Testing the 'time stamp'
+# On the obj.setTimeStamp, i gave it an empty space ('')
+	obj.setTimeStamp('')
+	print('What time is it? -> ' + obj.getTimeStamp())
 
 
 
---
+++ myWork/testComment.py
@@ -0,0 +1,9 @@
+# from comments import Comment
+
+# obj = Comment()
+
+# obj.setTimeStamp()
+
+# timeObj = obj.getTimeStamp()
+
+# print(timeObj)
