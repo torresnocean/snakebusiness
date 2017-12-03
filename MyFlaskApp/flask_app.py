ab3cda59f838b32b717b044d1957b7d987bb0d77
htorres6 <Humbertot31@gmail.com>
2017-12-02 18:52:06 -0800

    I got my FlaskApp DATABASE up and running

--- MyFlaskApp/flask_app.py
+++ MyFlaskApp/flask_app.py
@@ -1,7 +1,5 @@
-# A very simple Flask Hello World app for you to get started with...
-
 from flask import Flask, redirect, render_template, request, url_for
-#from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 #from flask_app import db
 
 #import time
@@ -12,29 +10,37 @@ app.config["DEBUG"] = True
 
 # configuring the database connection..
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = ('mysql+mysqlconnector://'
-#     + 'htorres6:pythonanywheremysql@htorres6.mysql.pythonanywhere-services.com'
-#     + '/htorres6$commentApp'
-#     )
-# app.config['SQLALCHEMY_POOL_RECYCLE'] = 299
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="htorres6",
+    password="chula_1-2-3",
+    hostname="htorres6.mysql.pythonanywhere-services.com",
+    databasename="htorres6$comments",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-# db = SQLAlchemy(app)
+db = SQLAlchemy(app)
 
-# class Comment(db.Model):
-#     __tablename__ = 'comments'
-#     id = db.Column(db.Integer, primary_key=True)
-#     content = db.Column(db.String(4096))
-#     timestamp = db.Column(db.String(256))
+class Comment(db.Model):
+	__tablename__ = 'comments'
+	
+	id = db.Column(db.Integer, primary_key=True)
+	content = db.Column(db.String(4096))
+	
+	#timestamp = db.Column(db.String(256))
 
-comments = []
+#comments = []
 
 @app.route("/", methods = ["GET", "POST"])
 def index():
     if request.method == "GET":
-        return render_template("index.html", comments = comments)
+        return render_template("index.html", comments=Comment.query.all())
 
-    comments.append(request.form["contents"])
+    comments = Comment(content=request.form["contents"])
+    db.session.add(comment)
+    adb.session.commit()
+
     return redirect(url_for('index'))
 
 # @app.route('/')
@@ -47,12 +53,6 @@ def about():
 
 
 #if __name__ == '__main__':
-
-
-
-
-
-
 
 
 # else:
